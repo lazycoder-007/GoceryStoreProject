334ae316059d464b5ef3fe1b39733833b137b626
Vikash Kumar Swami <vicky.swami18@gmail.com>
2020-04-30 20:52:26 +0530

    - Separated the concerns into separate files - created a new js file for handling grocery store options - removed the heading

--- frontEndFiles/mainGroceryStorePage.js
+++ frontEndFiles/mainGroceryStorePage.js
@@ -21,8 +21,6 @@ function displayStoreInventory()
 				}
 				list += "</ol>";
 				document.getElementById("idDisplayStore").innerHTML = list;
-				document.getElementById("idGroceryStoreOptions").style.display="none";
-				document.getElementById("idDisplayStore").style.display="block";
 			}
 		}
 		catch(erMs)
@@ -37,85 +35,15 @@ function displayStoreInventory()
 
 function openGroceryStoreOptionsPage()
 {
-	document.getElementById("idGroceryStoreOptions").style.display="block";
-	document.getElementById("idDisplayStore").style.display="none";	
+	location.replace("groceryStoreOptionsPage.html");
 }
 
 function displayCart()
 {
-	
+
 }
 
 function logOut()
 {
 	location.replace("loginPage.html");
-}
-
-function addItemToGorcery()
-{	
-	var brand = document.getElementById("idBrand").value;
-	var category = document.getElementById("idCategory").value;
-	var price = document.getElementById("idPrice").value;
-	var quantity = document.getElementById("idQuantity").value;
-	
-	if(brand == "" || category == "" || price == "" || quantity == "") 
-	{
-		window.alert("Values cannot be empty");
-		return;
-	}
-	
-	var groceryStoreUrl = "http://127.0.0.1:8080/groceryStore";
-	var jsonInput = JSON.stringify({"brand":brand,"category":category,"price":price,"stock":quantity});
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if(this.readyState == 4)
-		{
-			if(this.status == 200)
-			{
-				document.getElementById("idBrand").value="";
-				document.getElementById("idCategory").value="";
-				document.getElementById("idPrice").value="";
-				document.getElementById("idQuantity").value="";			
-			}
-			else
-			{
-				window.alert("Server is Down. Please try again@@@");
-			}
-		}
-	};
-	xhttp.open("POST", groceryStoreUrl, true);
-	xhttp.setRequestHeader('Content-Type', 'application/json');
-	xhttp.send(jsonInput);
-}
-
-function showAddItemToGrocery()
-{
-	document.getElementById("idUpdateItemPrice").style.display="none";	
-	document.getElementById("idUpdateItemQuantity").style.display="none";
-	document.getElementById("idAddItemToGorcery").style.display="block";
-}
-
-function showUpdateItemPrice()
-{
-	document.getElementById("idAddItemToGorcery").style.display="none";
-	document.getElementById("idUpdateItemQuantity").style.display="none";
-	document.getElementById("idUpdateItemPrice").style.display="block";
-}
-
-function showUpdateItemQuantity()
-{
-	document.getElementById("idAddItemToGorcery").style.display="none";
-	document.getElementById("idUpdateItemPrice").style.display="none";
-	document.getElementById("idUpdateItemQuantity").style.display="block";
-}
-
-function updateItemPrice()
-{
-	
-	
-}
-
-function updateItemQuantity()
-{
-	
 }
